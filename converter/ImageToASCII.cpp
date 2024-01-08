415027037d819eb30e258ae1d0f564a5a461b368
Aaron Storozinszki <115890722+Storiaron@users.noreply.github.com>
2024-01-08 14:47:16 +0100

    Create ImageConverter base class

---
+++ converter/ImageToASCII.cpp
@@ -0,0 +1,5 @@
+//
+// Created by User on 2024. 01. 08..
+//
+
+#include "ImageToASCII.h"
---
+++ converter/ImageToASCII.h
@@ -0,0 +1,14 @@
+//
+// Created by User on 2024. 01. 08..
+//
+
+#ifndef MAGIC_ASCII_ART_CONVERTER_IMAGETOASCII_H_
+#define MAGIC_ASCII_ART_CONVERTER_IMAGETOASCII_H_
+
+#include <string>
+class ImageToASCII {
+ public:
+  virtual std::string getASCIIString() = 0;
+};
+
+#endif //MAGIC_ASCII_ART_CONVERTER_IMAGETOASCII_H_
