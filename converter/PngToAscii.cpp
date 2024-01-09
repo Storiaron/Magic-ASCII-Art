0536b0cf17834c5fe7a9f604699252bfd048a142
Kristóf Németh <nemethkristof1@gmail.com>
2024-01-09 11:15:45 +0100

    implement load image

--- converter/PngToAscii.cpp
+++ converter/PngToAscii.cpp
@@ -1,8 +1,7 @@
-//
-// Created by k on 09/01/24.
-//
-
+#include <iostream>
 #include "PngToAscii.h"
+#include "../libraries/LodePNG/lodepng.h"
+
 std::string PngToAscii::getASCIIString() {
   return std::string();
 }
@@ -12,3 +11,10 @@ void PngToAscii::saveASCIItoFile() {
 std::string PngToAscii::guessFileType(std::string fileName) {
   return std::string();
 }
+
+void PngToAscii::loadImage() {
+  unsigned error = lodepng::decode(rawImage, width, height, filePath);
+  if(error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+}
+PngToAscii::PngToAscii(const std::string &file_path) : ImageToASCII(file_path) {}
+PngToAscii::~PngToAscii() = default;
--- converter/PngToAscii.h
+++ converter/PngToAscii.h
@@ -7,7 +7,15 @@
 #include "ImageToASCII.h"
 class PngToAscii : public ImageToASCII {
  public:
+  explicit PngToAscii(const std::string &file_path);
+  ~PngToAscii() override;
   std::string getASCIIString() override;
   void saveASCIItoFile() override;
   std::string guessFileType(std::string fileName) override;
+  void loadImage();
+ private:
+  std::vector<unsigned char> rawImage;
+  unsigned width;
+
+  unsigned height;
 };
