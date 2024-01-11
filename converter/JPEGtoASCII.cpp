541fc4f3ebce531bdc6b6261bc8efe658f4d2420
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2024-01-11 09:00:02 +0100

    add jpeg converter class

---
+++ converter/JPEGtoASCII.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tomi on 2024. 01. 10..
+//
+
+#include "JPEGtoASCII.h"
---
+++ converter/JPEGtoASCII.h
@@ -0,0 +1,11 @@
+//
+// Created by Tomi on 2024. 01. 10..
+//
+
+#pragma once
+
+#include "ImageToASCII.h"
+class JPEGtoASCII : public ImageToASCII {
+ public:
+ private:
+};
