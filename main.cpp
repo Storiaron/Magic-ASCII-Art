f695aafa6107256b5cdf3792b0423342538fa941
Kristóf Németh <nemethkristof1@gmail.com>
2024-01-11 11:03:33 +0100

    refactor main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include "libraries/EasyBMP/EasyBMP.h"
-#include "converter/PngToAscii.h"
 #include "App.h"
 
 int main(int argc, char *argv[]) {
-  App app;
-  app.run(argc, argv);
-  return EXIT_SUCCESS;
+  return App().run(argc, argv);
 }
